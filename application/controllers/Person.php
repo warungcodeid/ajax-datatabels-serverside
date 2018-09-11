de7e589798a11093168fc2111f266afdbc5c5a5f
Warung Code <warungcode.com@gmail.com>
2018-09-12 05:42:13 +0700

    Datatable

---
+++ application/controllers/Person.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Person extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('person_model','person');
+	}
+
+	public function index()
+	{
+		$this->load->helper('url');
+		$this->load->view('person_view');
+	}
+
+	public function ajax_list()
+	{
+		$list = $this->person->get_datatables();
+		$data = array();
+		$no = $_POST['start'];
+		foreach ($list as $person) {
+			$no++;
+			$row = array();
+			$row[] = $person->firstName;
+			$row[] = $person->lastName;
+			$row[] = $person->gender;
+			$row[] = $person->address;
+			$row[] = $person->dob;
+
+			//add html for action
+		
+			$data[] = $row;
+		}
+
+		$output = array(
+						"draw" => $_POST['draw'],
+						"recordsTotal" => $this->person->count_all(),
+						"recordsFiltered" => $this->person->count_filtered(),
+						"data" => $data,
+				);
+		//output to json format
+		echo json_encode($output);
+	}
+
+
+
+}
---
+++ application/views/person_view.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Warungcode</title>
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css')?>" rel="stylesheet">
+
+    </head>
+<body>
+    <div class="container">
+        <h1 style="font-size:20pt">Membuat Datatables with Server side Di Codeigniter</h1>
+
+        <h3>Person Data</h3>
+
+        <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Address</th>
+                    <th>Date of Birth</th>
+
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+
+            <tfoot>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>Date of Birth</th>
+
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+
+<script src="<?php echo base_url('assets/jquery/jquery-2.1.4.min.js')?>"></script>
+<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
+<script src="<?php echo base_url('assets/datatables/js/jquery.dataTables.min.js')?>"></script>
+<script src="<?php echo base_url('assets/datatables/js/dataTables.bootstrap.js')?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-datepicker/js/bootstrap-datepicker.min.js')?>"></script>
+
+
+<script type="text/javascript">
+
+var save_method; //for save method string
+var table;
+
+$(document).ready(function() {
+
+    //datatables
+    table = $('#table').DataTable({
+
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('person/ajax_list')?>",
+            "type": "POST"
+        },
+
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        {
+            "targets": [ -1 ], //last column
+            "orderable": false, //set not orderable
+        },
+        ],
+
+    });
+
+    //datepicker
+    $('.datepicker').datepicker({
+        autoclose: true,
+        format: "yyyy-mm-dd",
+        todayHighlight: true,
+        orientation: "top auto",
+        todayBtn: true,
+        todayHighlight: true,
+    });
+
+    //set input/textarea/select event when change value, remove class error and remove text help block
+    $("input").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+    $("textarea").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+    $("select").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+
+});
+
+
+
+
+
+function reload_table()
+{
+    table.ajax.reload(null,false); //reload datatable ajax
+}
+
+
+</script>
+
+</body>
+</html>
